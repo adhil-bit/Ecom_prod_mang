ab74c9f9b158664f0b1afbeea5fcfd890366f198
Adhil-bit <adhilashraf76@gmail.com>
2024-10-03 08:13:24 +0530

    subb category list with filter name with sub catagery added

--- controllers/subCategoryController.js
+++ controllers/subCategoryController.js
@@ -1,4 +1,5 @@
 const SubCategory = require('../models/subCategory');
+const { Op } = require('sequelize');
 
 exports.createSubCategory = async (req, res) => {
     try {
@@ -21,8 +22,21 @@ exports.createSubCategory = async (req, res) => {
 
 exports.getAllSubCategories = async (req, res) => {
     try {
-        const subCategory = await SubCategory.findAll();
-        res.json(subCategory);
+        const { name } = req.query;
+        console.log('name', name)
+        const whereClause = {};
+        if (name) {
+            whereClause.name = {
+                [Op.like]: `%${name}%` 
+            };
+        }
+        const subCategory = await SubCategory.findAndCountAll
+            ({
+                where: whereClause
+            });
+            console.log('subCategory.rows', subCategory.rows)
+
+        res.json(subCategory.rows);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
